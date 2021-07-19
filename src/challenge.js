bf0175ffba161893d61177f4511a6538d9b2b6a0
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-07-19 22:11:37 +0100

    Add challenge.js

---
+++ src/challenge.js
@@ -0,0 +1,233 @@
+const stores = [
+  {
+    location: "London",
+    products: [
+      {
+        name: "iPhone 6S",
+        type: "mobile",
+        stock: {
+          incomingDelivery: false,
+          quantity: 450,
+        },
+        price: 600,
+      },
+      {
+        name: "iPhone 10",
+        type: "mobile",
+        stock: {
+          incomingDelivery: false,
+          quantity: 20,
+        },
+        price: 1000,
+      },
+      {
+        name: "iPhone 12",
+        type: "mobile",
+        stock: {
+          incomingDelivery: true,
+          quantity: 0,
+        },
+        price: 1400,
+      },
+      {
+        name: "iPad Pro",
+        type: "tablet",
+        stock: {
+          incomingDelivery: true,
+          quantity: 100,
+        },
+        price: 600,
+      },
+      {
+        name: "iPad mini",
+        type: "tablet",
+        stock: {
+          incomingDelivery: false,
+          quantity: 300,
+        },
+        price: 600,
+      },
+      {
+        name: "MacBook Pro",
+        type: "computer",
+        stock: {
+          incomingDelivery: false,
+          quantity: 80,
+        },
+        price: 2400,
+      },
+      {
+        name: "MacBook Air",
+        type: "computer",
+        stock: {
+          incomingDelivery: false,
+          quantity: 200,
+        },
+        price: 1800,
+      },
+      {
+        name: "iMac",
+        type: "computer",
+        stock: {
+          incomingDelivery: true,
+          quantity: 15,
+        },
+        price: 2300,
+      },
+    ],
+  },
+  {
+    location: "Bristol",
+    products: [
+      {
+        name: "iPhone 6S",
+        type: "mobile",
+        stock: {
+          incomingDelivery: false,
+          quantity: 10,
+        },
+        price: 600,
+      },
+      {
+        name: "iPhone 10",
+        type: "mobile",
+        stock: {
+          incomingDelivery: false,
+          quantity: 200,
+        },
+        price: 1000,
+      },
+      {
+        name: "iPhone 12",
+        type: "mobile",
+        stock: {
+          incomingDelivery: true,
+          quantity: 45,
+        },
+        price: 1400,
+      },
+      {
+        name: "iPad Pro",
+        type: "tablet",
+        stock: {
+          incomingDelivery: true,
+          quantity: 30,
+        },
+        price: 600,
+      },
+      {
+        name: "iPad mini",
+        type: "tablet",
+        stock: {
+          incomingDelivery: false,
+          quantity: 300,
+        },
+        price: 600,
+      },
+      {
+        name: "MacBook Pro",
+        type: "computer",
+        stock: {
+          incomingDelivery: true,
+          quantity: 120,
+        },
+        price: 2400,
+      },
+      {
+        name: "MacBook Air",
+        type: "computer",
+        stock: {
+          incomingDelivery: false,
+          quantity: 0,
+        },
+        price: 1800,
+      },
+      {
+        name: "iMac",
+        type: "computer",
+        stock: {
+          incomingDelivery: true,
+          quantity: 150,
+        },
+        price: 2300,
+      },
+    ],
+  },
+  {
+    location: "Manchester",
+    products: [
+      {
+        name: "iPhone 6S",
+        type: "mobile",
+        stock: {
+          incomingDelivery: false,
+          quantity: 200,
+        },
+        price: 600,
+      },
+      {
+        name: "iPhone 10",
+        type: "mobile",
+        stock: {
+          incomingDelivery: true,
+          quantity: 50,
+        },
+        price: 1000,
+      },
+      {
+        name: "iPhone 12",
+        type: "mobile",
+        stock: {
+          incomingDelivery: true,
+          quantity: 0,
+        },
+        price: 1400,
+      },
+      {
+        name: "iPad Pro",
+        type: "tablet",
+        stock: {
+          incomingDelivery: false,
+          quantity: 75,
+        },
+        price: 600,
+      },
+      {
+        name: "iPad mini",
+        type: "tablet",
+        stock: {
+          incomingDelivery: false,
+          quantity: 0,
+        },
+        price: 600,
+      },
+      {
+        name: "MacBook Pro",
+        type: "computer",
+        stock: {
+          incomingDelivery: true,
+          quantity: 45,
+        },
+        price: 2400,
+      },
+      {
+        name: "MacBook Air",
+        type: "computer",
+        stock: {
+          incomingDelivery: false,
+          quantity: 200,
+        },
+        price: 1800,
+      },
+      {
+        name: "iMac",
+        type: "computer",
+        stock: {
+          incomingDelivery: true,
+          quantity: 0,
+        },
+        price: 2300,
+      },
+    ],
+  },
+];
